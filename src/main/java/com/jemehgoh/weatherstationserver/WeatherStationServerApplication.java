356f767b0a7a4233c2a305b290513058639b78cd
THINKPAD_01\jerem <jeremygoheh@gmail.com>
2025-05-29 11:22:54 +0800

    Initial commit

---
+++ src/main/java/com/jemehgoh/weatherstationserver/WeatherStationServerApplication.java
@@ -0,0 +1,13 @@
+package com.jemehgoh.weatherstationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStationServerApplication.class, args);
+	}
+
+}
