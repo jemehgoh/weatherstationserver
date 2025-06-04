08f2cd8d813a32b51929db05a75090a9f58f5cde
THINKPAD_01\jerem <jeremygoheh@gmail.com>
2025-06-04 12:17:34 +0800

    Change Controller to return readings as JSON

--- src/main/java/com/jemehgoh/weatherstationserver/WeatherStationController.java
+++ src/main/java/com/jemehgoh/weatherstationserver/WeatherStationController.java
@@ -1,6 +1,7 @@
 package com.jemehgoh.weatherstationserver;
 
 import com.jemehgoh.weatherstationserver.readings.Reading;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.ArrayList;
  * A REST controller that handles HTTP requests to the server.
  */
 
+@CrossOrigin
 @RestController
 public class WeatherStationController {
     private ArrayList<Reading> readings;
@@ -17,96 +19,22 @@ public class WeatherStationController {
         readings = new ArrayList<>();
     }
 
-    @GetMapping(value="/")
+    @GetMapping(value = "/")
     public String greetUser() {
         return "ESP32 weather station";
     }
 
-    @GetMapping(value="/data")
-    public String getData(@RequestParam(value = "type", defaultValue = "temperature") String dataType,
-            @RequestParam(value = "count", defaultValue = "10") int count) {
-        int readingCount = (readings.size() < count) ? readings.size() : count;
-        return getReadings(dataType, readingCount);
+    // Returns a JSON of the first reading in the reading list
+    @GetMapping(value = "/readings", produces = "application/json")
+    public ArrayList<Reading> getReadings() {
+        return readings;
     }
 
+    // Receives sensor data from the ESP32 through HTTP POST requests
     @PostMapping(value = "/post")
     public String postData(@RequestBody Reading reading) {
-       readings.add(reading);
-       return "Data received successfully.";
-    }
-
-    /**
-     * Returns a string with all requested readings, given the data type and reading count.
-     */
-    private String getReadings(String dataType, int readingCount) {
-        return switch(dataType.toLowerCase()) {
-            case "time" -> getTimeStamps(readingCount);
-            case "temperature" -> getTemperatureReadings(readingCount);
-            case "pressure" -> getPressureReadings(readingCount);
-            case "humidity" -> getHumidityReadings(readingCount);
-            case "co2ppm" -> getCo2PpmReadings(readingCount);
-            case "tvoc" -> getTvocReadings(readingCount);
-            default -> "Invalid data type";
-        };
-    }
-
-    private String getTimeStamps(int readingCount) {
-        String output = "Timestamps:\n";
-
-        for (int i = 0; i < readingCount; i++) {
-            output += String.format("%s: %s\n", "time", readings.get(i).time());
-        }
-
-        return output;
-    }
-
-    private String getTemperatureReadings(int readingCount) {
-        String output = "Readings:\n";
-
-        for (int i = 0; i < readingCount; i++) {
-            output += String.format("%s: %f\n", "temperature", readings.get(i).temperature());
-        }
-
-        return output;
-    }
-
-    private String getPressureReadings(int readingCount) {
-        String output = "Readings:\n";
-
-        for (int i = 0; i < readingCount; i++) {
-            output += String.format("%s: %f\n", "pressure", readings.get(i).pressure());
-        }
-
-        return output;
-    }
-
-    private String getHumidityReadings(int readingCount) {
-        String output = "Readings:\n";
-
-        for (int i = 0; i < readingCount; i++) {
-            output += String.format("%s: %f\n", "humidity", readings.get(i).humidity());
-        }
-
-        return output;
-    }
-
-    private String getCo2PpmReadings(int readingCount) {
-        String output = "Readings:\n";
-
-        for (int i = 0; i < readingCount; i++) {
-            output += String.format("%s: %d\n", "CO2 ppm", readings.get(i).co2Ppm());
-        }
-
-        return output;
-    }
-
-    private String getTvocReadings(int readingCount) {
-        String output = "Readings:\n";
-
-        for (int i = 0; i < readingCount; i++) {
-            output += String.format("%s: %d\n", "TVOC", readings.get(i).tvoc());
-        }
-
-        return output;
+        readings.add(reading);
+        return "Data received successfully.";
     }
 }
+
