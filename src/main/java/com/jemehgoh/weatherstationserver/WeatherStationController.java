17dcef904b046d1cd77881575e83afb977e37ab0
THINKPAD_01\jerem <jeremygoheh@gmail.com>
2025-06-08 09:23:48 +0800

    Add JSON file saving functionality

--- src/main/java/com/jemehgoh/weatherstationserver/WeatherStationController.java
+++ src/main/java/com/jemehgoh/weatherstationserver/WeatherStationController.java
@@ -1,9 +1,13 @@
 package com.jemehgoh.weatherstationserver;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jemehgoh.weatherstationserver.readings.Reading;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,7 +22,18 @@ public class WeatherStationController {
     private ArrayList<Reading> readings;
 
     public WeatherStationController() {
-        readings = new ArrayList<>();
+        try {
+            File dataFile = new File("data.json");
+            ObjectMapper mapper = new ObjectMapper();
+            if (!dataFile.createNewFile()) {
+                readings = mapper.readValue(dataFile, new TypeReference<ArrayList<Reading>>() {
+                });
+            }
+            System.out.println("Data file loaded");
+        } catch (IOException e) {
+            System.out.println("Unable to load data");
+            readings = new ArrayList<>();
+        }
     }
 
     @GetMapping(value = "/")
@@ -50,7 +65,20 @@ public class WeatherStationController {
     @PostMapping(value = "/post")
     public String postData(@RequestBody Reading reading) {
         readings.add(reading);
+        saveData();
         return "Data received successfully.";
+    }
+
+    private void saveData() {
+        try {
+            File dataFile = new File("data.json");
+            ObjectMapper mapper = new ObjectMapper();
+            if (dataFile.exists()) {
+                mapper.writeValue(dataFile, readings);
+            }
+        } catch (IOException e) {
+            System.out.println("Sensor data cannot be saved");
+        }
     }
 }
 
